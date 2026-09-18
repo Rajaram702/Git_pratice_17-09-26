4023730ebe1ecf3090a2d694a7eb582360442a83
Rajaram702 <144586167+Rajaram702@users.noreply.github.com>
2026-09-18 21:06:43 +0530

    Update Test.java
    Test class updated....

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
   public static void main(String[] args){
-       System.out.println("test class executed.......");
+       System.out.println("test class executed.........");
        System.out.println("Test class modified 18-09-26");
   }
 }
