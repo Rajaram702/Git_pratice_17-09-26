4468de968c1e09550708fb815ea3cec227660a19
Rajaram <rajaramrout001@gmail.com>
2026-09-18 21:01:50 +0530

    pratice class modified

--- Pratice.java
+++ Pratice.java
@@ -1,5 +1,5 @@
 class Pratice{
     public static void main(String[] args){
-        System.out.println("pratice class 18-09-26");
+        System.out.println("pratice class date at 18-09-26 ");
     }
 }
