3518caf5f074689a3186d678b253455b34cb3e46
Rajaram <rajaramrout001@gmail.com>
2026-09-18 21:30:33 +0530

    apple class updated from release branch

--- Apple.java
+++ Apple.java
@@ -1,5 +1,5 @@
 class Apple{
     public static void main(String[] args){
-        System.out.println("apple class created..");
+        System.out.println("apple class created***********");
     }
 }
